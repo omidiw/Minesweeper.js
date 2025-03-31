58900d79790ebb55367a57fe69126bd2c6f276b3
omidiw <omidghanbari.sh@gmail.com>
2025-04-01 03:19:07 +0330

    Delete scriptPractice.js
    wrong file just for practice was uploaded

--- scriptPractice.js
+++
@@ -1,9 +0,0 @@
-const friends = [
-  { name: "Anna", books: ["Bible", "Harry Potter"] },
-  { name: "Bob", books: ["War and peace", "Romeo and Juliet"] },
-  { name: "Alice", books: ["The Lord of the Rings", "The Shining"] },
-]
-const allBooks = friends.reduce((acc, cur) => [...acc, ...cur.books], [])
-console.log(allBooks)
-const allBooksFlatMap = friends.flatMap(person => person.books)
-console.log(allBooksFlatMap)
